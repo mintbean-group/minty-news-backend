a2bc40e0669c6d8faf10a74c11681b41d9fe68e8
Ania-M-Pienio <aniapienio@gmail.com>
2020-05-02 16:39:26 -0400

    Fresh Frontend

---
+++ public/precache-manifest.f855118dd90bb4886691c3ba462f2014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9c948c928cadc6280bcfc2e7f377f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7e30e1b7be7095e810",
+    "url": "/static/css/main.438d19bf.chunk.css"
+  },
+  {
+    "revision": "8172fb2532863a1cbc04",
+    "url": "/static/js/2.11031a36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11031a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7e30e1b7be7095e810",
+    "url": "/static/js/main.878c51c0.chunk.js"
+  },
+  {
+    "revision": "867757405b3ff4687d33",
+    "url": "/static/js/runtime-main.4608bf12.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);

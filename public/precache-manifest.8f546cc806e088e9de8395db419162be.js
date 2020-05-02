d9a0039c4c504148b1e525d312ee25eab583d960
Ania-M-Pienio <aniapienio@gmail.com>
2020-05-02 18:55:38 -0400

    fresh frontend

--- public/precache-manifest.8f546cc806e088e9de8395db419162be.js
+++ public/precache-manifest.8f546cc806e088e9de8395db419162be.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7302c0d1b1a4e6592af2c9fe7790763",
+    "revision": "ec602020148d8fb259d2c6c433fee7df",
     "url": "/index.html"
   },
   {
-    "revision": "be907696a00d8b9b1fe6",
+    "revision": "a15df1a382abad60e9c0",
     "url": "/static/css/main.438d19bf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.75519859.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be907696a00d8b9b1fe6",
-    "url": "/static/js/main.a646d5af.chunk.js"
+    "revision": "a15df1a382abad60e9c0",
+    "url": "/static/js/main.faba1d02.chunk.js"
   },
   {
     "revision": "867757405b3ff4687d33",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.558fdce77979500d7456b8ec6dacbb99.js"
+  "/precache-manifest.8f546cc806e088e9de8395db419162be.js"
 );
 
 self.addEventListener('message', (event) => {
